d4fccdb92564dd98ec4803c99a58a8758b691071
nmamula <NMamula2805>
2022-01-24 10:26:24 +0100

    first commit

---
+++ myLibrary/src/main/java/com/example/mylibrary/simpleCalculation.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+public class simpleCalculation {
+
+    private int calculate(int x, int y){
+        int res;
+        res = x*y;
+        return res;
+    }
+
+}
